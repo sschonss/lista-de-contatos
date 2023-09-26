070f55277f6c100bdc6061928f8f6ad4781b7996
sschonss <schonsluuiz@gmail.com>
2023-09-26 19:09:19 -0300

    Start Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importe FormsModule
+import { HttpClientModule } from '@angular/common/http'; // Importe HttpClientModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaContatosComponent } from './lista-contatos/lista-contatos.component';
+import { AdicionarContatoComponent } from './adicionar-contato/adicionar-contato.component';
+import { EditarContatoComponent } from './editar-contato/editar-contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaContatosComponent,
+    AdicionarContatoComponent,
+    EditarContatoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // Adicione FormsModule aqui
+    HttpClientModule, // Adicione HttpClientModule aqui
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
